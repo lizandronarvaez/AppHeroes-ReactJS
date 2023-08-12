af42b0d1c2a94ecefce2e5e77f7f580f031aa1e6
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2023-08-12 18:09:17 +0200

    Añadiendo el componente footer al Componente principal

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,7 +3,7 @@ import Login from '../auth/pages/Login'
 import HeroesRoutes from '../heroes/routes/HeroesRoutes'
 import PrivateRoute from './PrivateRoute'
 import PublicRoute from './PublicRoute'
-import Footer from '../interface/components/NavBar/Footer/Footer'
+import Footer from '../interface/components/Footer/Footer'
 
 const AppRouter = () => {
     return (
