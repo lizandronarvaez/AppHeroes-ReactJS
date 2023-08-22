72697e2b67d325dc0ddbd795271c284740970aaa
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2023-08-22 12:31:44 +0200

    Solucionando error que impedia la busqueda al utilizar tildes

--- src/heroes/helpers/getHeroesByName.js
+++ src/heroes/helpers/getHeroesByName.js
@@ -5,7 +5,15 @@ const getHeroesByName = (name = "") => {
     if (name.length === 0) {
         return [];
     }
-    return heroes.find(heroe => heroe.superhero.toLowerCase().trim().replace(/ /g, "").includes(name.replace(/ /g, "")))
+    return heroes.find(heroe =>
+        heroe.superhero
+            .normalize("NFD")
+            .toLowerCase()
+            .trim()
+            .replace(/[\u0300-\u036f]/g, "")
+            .includes(
+                name.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+            ))
 }
 
 export default getHeroesByName
